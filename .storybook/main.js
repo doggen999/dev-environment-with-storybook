6a3f9d08b4aa18a5aa47e722f7839bee11024527
Natan Williams <natan.williams123@gmail.com>
2021-07-01 18:44:01 +0200

    Add Storybook with default configuration

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const webpackConfig = require("../webpack.config");
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: { ...config.module, rules: webpackConfig.module.rules },
+    };
+  },
+};
