b9fe9a18e7744ac68e48b12f0160bd8771aace72
Natan Williams <natan.williams123@gmail.com>
2021-08-01 23:14:18 +0200

    Fix Remove example stories from rendering

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,12 @@
 const path = require("path");
 const webpackConfig = require("../webpack.config");
+
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    // "../src/**/*.stories.mdx",
+    // "../src/**/*.stories.@(jsx)",
+    "../src/**/*.stories.@(js|ts|tsx)",
+  ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {
     builder: "webpack5",
